67c67d56bb5fd51c4023936e7dd8771e037cd321
James Houck <jameshouck@live.com>
2019-11-07 13:53:44 -0600

    added place form, added to template

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Place
+from .forms import NewPlaceForm
 
 # Create your views here.
 
 def place_list(request):
-    return render(request, 'travel_wishlist/wishlist.html')
+    places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
