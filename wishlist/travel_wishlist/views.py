2a31d363a6820c1c5d726a984889ec245f3d2c14
James Houck <jameshouck@live.com>
2019-11-07 13:57:27 -0600

    handled post request in place_list to save new row

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -5,6 +5,17 @@ from .forms import NewPlaceForm
 # Create your views here.
 
 def place_list(request):
+
+    #handle post request to method and create the place
+    if request.method == 'POST':
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid():
+            place.save()
+            return redirect('place_list')
+
+
+    #when not a post request, just show the form with all unvistited places
     places = Place.objects.filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
