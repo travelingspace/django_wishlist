a29af416fa9fb3823d4e35b71553c68d79732032
James Houck <jameshouck@live.com>
2019-11-13 16:03:59 -0600

    user id added to place on save

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -11,20 +11,21 @@ def place_list(request):
     #handle post request to method and create the place
     if request.method == 'POST':
         form = NewPlaceForm(request.POST)
-        place = form.save()
+        place = form.save(commit=False)
+        place.user = request.user
         if form.is_valid():
             place.save()
             return redirect('place_list')
 
 
     #when not a post request, just show the form with all unvistited places
-    places = Place.objects.filter(visited=False).order_by('name')
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
 @login_required
 def places_visited(request):
-    visited = Place.objects.filter(visited=True)
+    visited = Place.objects.filter(user=request.user).filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
 
 @login_required
